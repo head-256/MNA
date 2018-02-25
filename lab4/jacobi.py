24ea2e5eec8a7e26d07b46d29692ac296ef27b9e
Dubhad <dubhad.connect@gmail.com>
2018-02-26 00:26:12 +0300

    Some style improvements

--- lab4/jacobi.py
+++ lab4/jacobi.py
@@ -1,76 +1,100 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+
 from math import sqrt
+from copy import deepcopy
 from numpy import array, identity, diagonal
 
 
-def maxElem(a):
-    n = len(a)
-    aMax = 0.0
+def max_elem(A):  # Нахождение максимального элемента в верхнем треугольнике матрицы A: A[k, l]
+    n = len(A)
+    a_max = 0.0
+    k = l = 0.0
+
     for i in range(n - 1):
         for j in range(i + 1, n):
-            if abs(a[i, j]) >= aMax:
-                aMax = abs(a[i, j])
+            if abs(A[i, j]) >= a_max:
+                a_max = abs(A[i, j])
                 k = i
                 l = j
-    return aMax, k, l
+    return a_max, k, l
 
 
-def rotate(a, p, k, l):
-        n = len(a)
-        aDiff = a[l, l] - a[k, k]
-        if abs(a[k, l]) < abs(aDiff) * 1.0e-36:
-            t = a[k, l] / aDiff
-        else:
-            phi = aDiff / (2.0 * a[k, l])
-            t = 1.0 / (abs(phi) + sqrt(phi**2 + 1.0))
-            if phi < 0.0:
-                t = -t
-        c = 1.0 / sqrt(t**2 + 1.0)
-        s = t * c
-        tau = s / (1.0 + c)
-        temp = a[k, l]
-        a[k, l] = 0.0
-        a[k, k] = a[k, k] - t * temp
-        a[l, l] = a[l, l] + t * temp
-        for i in range(k):
-            temp = a[i, k]
-            a[i, k] = temp - s * (a[i, l] + tau * temp)
-            a[i, l] = a[i, l] + s * (temp - tau * a[i, l])
-        for i in range(k + 1, l):
-            temp = a[k, i]
-            a[k, i] = temp - s * (a[i, l] + tau * a[k, i])
-            a[i, l] = a[i, l] + s * (temp - tau * a[i, l])
-        for i in range(l + 1, n):
-            temp = a[k, i]
-            a[k, i] = temp - s * (a[l, i] + tau * temp)
-            a[l, i] = a[l, i] + s * (temp - tau * a[l, i])
-        for i in range(n):
-            temp = p[i, k]
-            p[i, k] = temp - s * (p[i, l] + tau * p[i, k])
-            p[i, l] = p[i, l] + s * (temp - tau * p[i, l])
+def rotate(A, P, k, l):  # t - tan, s - sin, c - cos
+    n = len(A)
+    a_diff = A[l, l] - A[k, k]
 
-def jacobi(a, tol = 1.0e-9):
-    n = len(a)
-    maxRot = 5 * (n**2)
-    p = identity(n) * 1.0
-    for i in range(maxRot):
-        aMax, k, l = maxElem(a)
-        print(aMax)
-        if aMax < tol:
-            return diagonal(a), p
-        rotate(a, p, k, l)
-    print("Jacobi method did not converge")
+    if abs(A[k, l]) < abs(a_diff) * 1.0e-36:  # если A[k,l] очень мало
+        t = A[k, l] / a_diff
+    else:
+        phi = a_diff / (2.0 * A[k, l])
+        t = 1.0 / (abs(phi) + sqrt(phi ** 2 + 1.0))
+        if phi < 0.0:
+            t = -t
+    c = 1.0 / sqrt(t ** 2 + 1.0)
+    s = t * c
+    tau = s / (1.0 + c)
+    temp = A[k, l]
+    A[k, l] = 0.0
+    A[k, k] = A[k, k] - t * temp
+    A[l, l] = A[l, l] + t * temp
+    for i in range(k):  # Если i < k
+        temp = A[i, k]
+        A[i, k] = temp - s * (A[i, l] + tau * temp)
+        A[i, l] = A[i, l] + s * (temp - tau * A[i, l])
+    for i in range(k + 1, l):  # Если k < i < l
+        temp = A[k, i]
+        A[k, i] = temp - s * (A[i, l] + tau * A[k, i])
+        A[i, l] = A[i, l] + s * (temp - tau * A[i, l])
+    for i in range(l + 1, n):  # Если i > l
+        temp = A[k, i]
+        A[k, i] = temp - s * (A[l, i] + tau * temp)
+        A[l, i] = A[l, i] + s * (temp - tau * A[l, i])
+    for i in range(n):
+        temp = P[i, k]
+        P[i, k] = temp - s * (P[i, l] + tau * P[i, k])
+        P[i, l] = P[i, l] + s * (temp - tau * P[i, l])
 
 
-A = array([[2.0, 1.0],
-          [1.0, 3.0]])
-B = array([[5.0, 1.0, 2.0],
-          [1.0, 4.0, 1.0],
-          [2.0, 1.0, 3.0]])
-C = array([[2.979, 0.273, 0.318, 0.219],
-           [0.273, 3.951, 0.197, 0.231],
-           [0.318, 0.197, 2.875, 0.187],
-           [0.219, 0.231, 0.187, 3.276]])
+def jacobi(A, eps):
+    A = deepcopy(A)
+    n = len(A)
+    max_rot = 5 * (n ** 2)
+    P = identity(n)  # Матрица вращений
 
-print(jacobi(A, tol=0.01))
-print(jacobi(B, tol=0.01))
-print(jacobi(C, tol=0.01))
+    for i in range(max_rot):
+        a_max, k, l = max_elem(A)
+        if a_max < eps:
+            return diagonal(A), P
+        rotate(A, P, k, l)
+    return None
+
+
+def transpose(A):
+    n = len(A)
+    T = deepcopy(A)
+    for i in range(n):
+        for j in range(n):
+            if i > j:
+                T[i][j], T[j][i] = T[j][i], T[i][j]
+    return T
+
+
+if __name__ == "__main__":
+    A = array([[2.0, 1.0],
+               [1.0, 3.0]])
+    B = array([[5.0, 1.0, 2.0],
+               [1.0, 4.0, 1.0],
+               [2.0, 1.0, 3.0]])
+    C = array([[2.979, 0.273, 0.318, 0.219],
+               [0.273, 3.951, 0.197, 0.231],
+               [0.318, 0.197, 2.875, 0.187],
+               [0.219, 0.231, 0.187, 3.276]])
+
+    res_a = jacobi(A, 0.01)
+    res_b = jacobi(B, 0.01)
+    res_c = jacobi(C, 0.01)
+
+    print("Eigenvalues: {}\nEigenvectors:\n{}".format(res_a[0], transpose(res_a[1])))
+    print("Eigenvalues: {}\nEigenvectors:\n{}".format(res_b[0], transpose(res_b[1])))
+    print("Eigenvalues: {}\nEigenvectors:\n{}".format(res_c[0], transpose(res_c[1])))
