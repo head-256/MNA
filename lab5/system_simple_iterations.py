bcfbab4263dadd1153c8e4c949fb43c0de20a489
Dubhad <dubhad.connect@gmail.com>
2018-04-24 23:04:24 +0300

    Added working system simple iterations method

--- lab5/system_simple_iterations.py
+++ lab5/system_simple_iterations.py
@@ -4,42 +4,34 @@
 import numpy as np
 
 
-def f_1(values):
-    return np.arctan(values[0] ** 2 - 0.5) / values[0]
+def f1_plus(y):
+    return np.sqrt(1 - (y ** 2))
 
 
-def f_2_plus(values):
-    return np.sqrt(1 - (values[1] ** 2))
+def f1_minus(y):
+    return -np.sqrt(1 - (y ** 2))
 
 
-def f_2_minus(values):
-    return -np.sqrt(1 - (values[1] ** 2))
+def f2(x):
+    return (np.arctan(x ** 2) - 0.5) / x
 
 
-def precision(solution0, solution_prev, eps=0.001):
-    for i in range(len(solution0)):
-        if abs(solution0[i] - solution_prev[i]) > eps:
-            return False
-    return True
+def simple_iterations_method(f1, f2, x, y, eps):
+    x_value = f1(y)
+    y_value = f2(x)
 
-
-def simple_iterations_method(functions, solution_start):
-    solution_prev = solution_start[:]
-    solution0 = solution_start[:]
-    for i in range(len(solution_prev)):
-        solution_prev[i] += 1
-
-    while not precision(solution0, solution_prev):
-        solution_prev = solution0[:]
-        for i in range(len(solution0)):
-            solution0[i] = functions[i](solution0)
-    return solution0
+    while max(abs(x_value - x), abs(y_value - y)) > eps:
+        x = f1(y_value)
+        y = f2(x_value)
+        x_value = f1(y)
+        y_value = f2(x)
+    return x, y
 
 
 if __name__ == "__main__":
 
-    solution_plus = simple_iterations_method([f_2_plus, f_1], [0.9, 0.3])
-    solution_minus = simple_iterations_method([f_2_minus, f_1], [-0.9, -0.3])
-    solution = solution_plus + solution_minus
+    result_1 = list(simple_iterations_method(f1_plus, f2, x=0.9, y=0.3, eps=0.001))
+    result_2 = list(simple_iterations_method(f1_minus, f2, x=-0.9, y=-0.3, eps=0.001))
 
-    print("Result: {roots}".format(roots=solution))
+    print('Result:')
+    print(result_1, result_2, sep='\n')
