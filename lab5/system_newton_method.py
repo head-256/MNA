d1eb1816a9af9c985a4447badd5c773eb3034a60
Dubhad <dubhad.connect@gmail.com>
2018-04-24 19:43:13 +0300

    Added mod system Newton method

--- lab5/system_newton_method.py
+++ lab5/system_newton_method.py
@@ -25,6 +25,18 @@ def newton_method(f, J, x, eps):
     return x
 
 
+def newton_method_mod(f, J, x, eps):
+    f_value = f(x)
+    f_norm = np.linalg.norm(f_value, ord=2)
+    jacobian = J(x)
+    while abs(f_norm) > eps:
+        delta = np.linalg.solve(jacobian, -f_value)
+        x += delta
+        f_value = f(x)
+        f_norm = np.linalg.norm(f_value, ord=2)
+    return x
+
+
 def f(x):
     return np.array(
         [np.tan(x[0] * x[1] + 0.5) - x[0] ** 2,
@@ -44,6 +56,14 @@ if __name__ == "__main__":
     result_3 = newton(np.array([0.4, -1]))
     result_4 = newton(np.array([-0.4, 1]))
 
-    print('Result:')
+    print('Result (Newton Method):')
     print(result_1, result_2, result_3, result_4, sep='\n')
 
+    newton_mod = partial(newton_method_mod, f, J, eps=0.001)
+    result_5 = newton_mod(np.array([0.9, 0.3]))
+    result_6 = newton_mod(np.array([-0.9, -0.3]))
+    result_7 = newton_mod(np.array([0.4, -1]))
+    result_8 = newton_mod(np.array([-0.4, 1]))
+
+    print('Result (Mod Newton Method):')
+    print(result_5, result_6, result_7, result_8, sep='\n')
