2f1c03e6b5c24d97cb720478b21aea399d09027e
Dubhad <dubhad.connect@gmail.com>
2018-04-28 11:21:48 +0300

    Added Simpson method (lab7)

--- lab7/simpson.py
+++ lab7/simpson.py
@@ -2,16 +2,19 @@
 # -*- coding: utf-8 -*-
 
 
-def trapezoid_method(f, a, b, n):
+def simpson(f, a, b, n):
     if n % 2:
         raise ValueError("n must be even")
 
-    h = (b - a) / float(n)
+    h = (b - a) / n
     print(h)
-    s = 0.5 * (f(a) + f(b))
-    for i in range(1, n):
-        s += f(a + i * h)
-    return h * s
+    s = f(a) + f(b)
+    for i in range(1, n, 2):
+        s += 4 * f(a + i * h)
+    for i in range(2, n-1, 2):
+        s += 2 * f(a + i * h)
+
+    return s * h / 3
 
 
 def f(x):
@@ -22,7 +25,7 @@ if __name__ == '__main__':
     a = 0
     b = 2
     n = 20
-    result_h = trapezoid_method(f, a, b, n)
-    result_2h = trapezoid_method(f, a, b, n // 2)
+    result_h = simpson(f, a, b, n)
+    result_2h = simpson(f, a, b, n // 2)
     print("Result with h steps: {result}".format(result=result_h))
     print("Result with 2h steps: {result}".format(result=result_2h))
